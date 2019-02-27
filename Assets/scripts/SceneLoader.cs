d5e39328f2cdc7bab06c5cc45523c27650c404e3
Kiraay <Habakuk707@gmx.at>
2019-02-27 17:33:08 +0100

    add gitignore and assets

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
+
